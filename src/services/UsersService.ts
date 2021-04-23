1c3a9182612143c2d909af4971624618585ddf5c
JMustang <eecfredes@gmail.com>
2021-04-23 17:49:45 -0300

    feat:message table criada

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,21 +1,29 @@
-import { getCustomRepository } from "typeorm";
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
 import { UsersRepository } from "../repositories/UsersRepository";
 
 class UserService {
+  private usersRepository: Repository<User>;
+
+  constructor() {
+    this.usersRepository = getCustomRepository(UsersRepository);
+  }
+
   async create(email: string) {
-    const usersRepository = getCustomRepository(UsersRepository);
     // Verificar se o Usuario existe
-    const userExists = await usersRepository.findOne({});
+    const userExists = await this.usersRepository.findOne({
+      email,
+    });
     // se nao existe, salvar no DB
     if (userExists) {
       return userExists;
     }
     // se existe, retorna user
-    const user = usersRepository.create({
+    const user = this.usersRepository.create({
       email,
     });
 
-    await usersRepository.save(user);
+    await this.usersRepository.save(user);
 
     return user;
   }
